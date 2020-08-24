562ee4276a1e5b47418c8e2e1c55eae79e3f45b6
Bianca <cxbianca@yahoo.com>
2020-08-24 09:26:44 +0300

    First commit

--- src/Stock.js
+++ src/Stock.js
@@ -1,5 +1,7 @@
 import React from "react";
+import './Stock.css';
 import {Line} from 'react-chartjs-2';
+
 let chart = {
     labels: [],
     datasets: [
@@ -14,15 +16,15 @@ let chart = {
             borderDashOffset: 0.0,
             borderJoinStyle: 'miter',
             pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
+            pointBackgroundColor: '#1c49ff',
             pointBorderWidth: 1,
             pointHoverRadius: 5,
             pointHoverBackgroundColor: 'rgba(75,192,192,1)',
             pointHoverBorderColor: 'rgba(220,220,220,1)',
             pointHoverBorderWidth: 2,
             pointRadius: 1,
-            pointHitRadius: 10,
-            data: []
+            pointHitRadius: 20,
+            data: [],
         }
     ]
 };
@@ -30,14 +32,13 @@ class Stock extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            axisX: [],
-            axisY: [],
             inputSymbol: 'FB',
-            chartData: []
+            selectedOption: 'default',
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.change = this.change.bind(this);
     }
 
     handleChange(event) {
@@ -45,8 +46,13 @@ class Stock extends React.Component{
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.inputSymbol);
+        // alert('A name was submitted: ' + this.state.inputSymbol);
         event.preventDefault();
+        this.fetchStock();
+    }
+
+    change(event){
+        this.setState({selectedOption: event.target.value});
         this.fetchStock();
     }
 
@@ -55,17 +61,15 @@ class Stock extends React.Component{
     }
 
     fetchStock(){
-
-        let title = '';
         const parent = this;
         const apiKey = '5CIMSF66Q8D8J60O';
+        let title = '';
         let stockChartXValuesFunction = [];
         let stockChartYValuesFunction = [];
-        let dataChart ={};
         let symbol = parent.state.inputSymbol;
-        console.log(parent.state.inputSymbol);
         let apiCall = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${symbol}&outputsize=compact&apikey=${apiKey}`;
-        console.log(apiCall);
+        let i = 0;
+        // let contor = 0;
 
         fetch(apiCall)
             .then(
@@ -75,21 +79,33 @@ class Stock extends React.Component{
             )
             .then(
                 function(data) {
-                // console.log(data, 'api data')
-                let i = 0;
+                    console.log(data);
+                   if(parent.state.selectedOption === '10'){
+                       i = 10;
+                   } else if(parent.state.selectedOption === '50'){
+                       i = 50;
+                   } else if(parent.state.selectedOption === 'default'){
+                       i = 100;
+                   }
+                    console.log(i);
+                    // let dataArr = [];
+                    // let dataArrVal = [];
                     for (var key in data['Time Series (Daily)']) {
-                        var dataArr = [];
-                        // console.log(new Date(key))
                         // dataArr.push(key);
+                        // dataArrVal.push(parseFloat(data['Time Series (Daily)'][key]['1. open']));
                         chart['labels'].push(key);
                         chart['datasets'][0]['data'].push(parseFloat(data['Time Series (Daily)'][key]['1. open']));
-                        // dataArr.push(parseFloat(data['Time Series (Daily)'][key]['1. open']));
-                        // chartDataComp.push(dataArr);
                     }
+                    // for(let j =0; j<i; j++){
+                    //     chart['labels'].push(dataArr[j]);
+                    //     chart['datasets'][0]['data'].push(dataArrVal[j]);
+                    // }
+
+                    console.log(chart);
+
                     title ='Stock Price for '+ parent.state.inputSymbol;
                     chart['datasets'][0]['label']= title;
-                    // parent.setState({chartData: chartDataComp});
-                    console.log(dataChart);
+
                     parent.setState({
                         axisX: stockChartXValuesFunction,
                         axisY: stockChartYValuesFunction
@@ -101,19 +117,40 @@ class Stock extends React.Component{
 
     render(){
         return (
-            <div>
-                <h4>Hellothere!</h4>
-                <div id="chart" style={{width: "50%", margin: "20px 20px"}}>
-                    <Line data={chart} />
+            <div className="stock">
+                <div className="fixed">
+                    <h1 id="title">Welcome to Stock Web App!</h1>
                 </div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name:
-                        <input type="text" value={this.state.inputSymbol} onChange={this.handleChange.bind(this)} />
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
-                <h1>{this.state.inputSymbol}</h1>
+
+                <div className="row">
+                    <div className="col-8">
+                        <div id="chart">
+                            <Line data={chart} />
+                        </div>
+                    </div>
+
+                    <div className="col-4">
+                        <h4 className="symbol">Your stock symbol is: {this.state.inputSymbol}</h4>
+                        <form  className="form" onSubmit={this.handleSubmit}>
+                            <div  className="form">
+                                <label>
+                                    Enter another symbol:
+                                </label>
+                            </div>
+
+                            <div className="form">
+                                <input type="text"  className="form-control" placeholder="Symbol" onChange={this.handleChange.bind(this)} />
+                            </div>
+
+                            <p id="text" className="symbol"> Maybe you want to change the number of days</p>
+                            <select className="form-control" id="exampleFormControlSelect1" onChange={this.change.bind(this)} value={this.state.selectedOption}>
+                                <option value="default">Default</option>
+                                <option value="10">10 days</option>
+                                <option value="50">50 days</option>
+                            </select>
+                        </form>
+                    </div>
+                </div>
             </div>
         );
     }
